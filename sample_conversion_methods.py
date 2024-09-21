602bc2d7735f5724e3063ffb27b510cd16fc3d2c
cuppajoeman <ccn@cuppajoeman.com>
2024-09-21 13:32:15 -0400

    feat: add breadcrumb navigation to sample converter

--- sample_conversion_methods.py
+++ sample_conversion_methods.py
@@ -1,4 +1,25 @@
 from typing import Dict, Callable
+import os
+
+def generate_breadcrumb(path_to_content_file: str) -> str:
+    """
+    Generates breadcrumb navigation from the file path.
+    Example: path_to_content_file 'generated_html/a/b/c/file.html'
+    would generate the breadcrumb as:
+    '~/a/b/c/' where a, b, c are clickable and link to /a/index.html, /a/b/index.html, etc.
+    """
+    breadcrumb_html = '<nav class="breadcrumb">\n'
+    parts = path_to_content_file.replace("generated_html/", "").split("/")[:-1]  # exclude the file name
+    current_path = "/"
+    
+    breadcrumb_html += f'<a href="/index.html">~</a>'
+
+    for i, part in enumerate(parts):
+        current_path += part + "/"
+        breadcrumb_html += f'/<a href="{current_path}index.html">{part}</a>'
+
+    breadcrumb_html += "\n</nav>\n"
+    return breadcrumb_html
 
 def toolbox_template_conversion(path_to_content_file: str, file_name: str, template_file: str):
     """
@@ -17,16 +38,21 @@ def toolbox_template_conversion(path_to_content_file: str, file_name: str, templ
     header_line_index = next(i for i, s in enumerate(template_lines) if "HEADER TITLE" in s)
     template_lines[header_line_index] = template_lines[header_line_index].replace("HEADER TITLE", page_title)
 
+    print(f"DEBUG PATH TO CONTENT FILE {path_to_content_file}")
+    # Generate breadcrumb navigation
+    breadcrumb_html = generate_breadcrumb(path_to_content_file)
+
     with open(path_to_content_file, "r") as f:
         content_string = f.read()
 
+    # Insert breadcrumb navigation above the main content
     main_content_area_index = next(i for i, s in enumerate(template_lines) if "CONTENT" in s)
-    template_lines[main_content_area_index] = template_lines[main_content_area_index].replace("CONTENT", content_string)
+    template_lines[main_content_area_index] = breadcrumb_html + template_lines[main_content_area_index].replace("CONTENT", content_string)
 
     link_to_file_on_git_index = next(i for i, s in enumerate(template_lines) if "FILENAME" in s)
     # because we do this on a different directory we remove the temporary directory from path
     corrected_path = path_to_content_file.replace("generated_html/", "")
-    template_lines[link_to_file_on_git_index] = template_lines[link_to_file_on_git_index].replace("FILENAME", corrected_path);
+    template_lines[link_to_file_on_git_index] = template_lines[link_to_file_on_git_index].replace("FILENAME", corrected_path)
 
     with open(path_to_content_file, "w") as f:
         contents = "".join(template_lines)
@@ -36,4 +62,3 @@ def toolbox_template_conversion(path_to_content_file: str, file_name: str, templ
 template_file_to_conversion : Dict[str, Callable[[str, str, str], None]] = {
     "toolbox_template.html": toolbox_template_conversion
 }
-
